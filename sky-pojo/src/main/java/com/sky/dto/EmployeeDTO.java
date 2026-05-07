be3c3c3556fc57b4c096e695edd20645715159e2
zhiyong-creator <2914825922@qq.com>
2026-05-07 20:01:54 +0800

    feat(employee): 添加员工管理功能
    - 新增员工DTO类并配置Swagger文档注解
    - 在EmployeeController中添加员工登录、退出和新增接口
    - 实现EmployeeService中的save方法处理员工新增逻辑
    - 配置EmployeeMapper的insert操作映射数据库
    - 添加用户名重复检查和默认密码设置功能
    - 更新开发环境数据库密码配置为123456

--- sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
+++ sky-pojo/src/main/java/com/sky/dto/EmployeeDTO.java
@@ -1,22 +1,31 @@
 package com.sky.dto;
 
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
 
 @Data
+@ApiModel(description = "新增员工时传递的数据模型")
 public class EmployeeDTO implements Serializable {
 
+    @ApiModelProperty("主键值")
     private Long id;
 
+    @ApiModelProperty("用户名")
     private String username;
 
+    @ApiModelProperty("姓名")
     private String name;
 
+    @ApiModelProperty("手机号")
     private String phone;
 
+    @ApiModelProperty("性别")
     private String sex;
 
+    @ApiModelProperty("身份证号")
     private String idNumber;
 
 }
--- sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/EmployeeServiceImpl.java
@@ -1,18 +1,26 @@
 package com.sky.service.impl;
 
 import com.sky.constant.MessageConstant;
+import com.sky.constant.PasswordConstant;
 import com.sky.constant.StatusConstant;
+import com.sky.dto.EmployeeDTO;
 import com.sky.dto.EmployeeLoginDTO;
 import com.sky.entity.Employee;
+import com.sky.exception.AccountAlreadyExistsException;
 import com.sky.exception.AccountLockedException;
 import com.sky.exception.AccountNotFoundException;
 import com.sky.exception.PasswordErrorException;
 import com.sky.mapper.EmployeeMapper;
 import com.sky.service.EmployeeService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.util.DigestUtils;
 
+import java.time.LocalDateTime;
+
+@Slf4j
 @Service
 public class EmployeeServiceImpl implements EmployeeService {
 
@@ -54,5 +62,35 @@ public class EmployeeServiceImpl implements EmployeeService {
         //3、返回实体对象
         return employee;
     }
+
+
+    public void save(EmployeeDTO employeeSaveDTO) {
+        log.info("开始新增员工，用户名：{}", employeeSaveDTO.getUsername());
+        
+        // 检查用户名是否已存在
+        Employee employee = employeeMapper.getByUsername(employeeSaveDTO.getUsername());
+        log.info("查询结果：{}", employee == null ? "用户名不存在，可以新增" : "用户名已存在，员工ID=" + employee.getId());
+        
+        if (employee != null) {
+            // 用户名已存在，抛出异常
+            throw new AccountAlreadyExistsException(MessageConstant.USERNAME_ALREADY_EXISTS);
+        }
+
+        Employee newEmployee = new Employee();
+        BeanUtils.copyProperties(employeeSaveDTO, newEmployee);
+
+        newEmployee.setPassword(DigestUtils.md5DigestAsHex(PasswordConstant.DEFAULT_PASSWORD.getBytes()));
+
+        newEmployee.setStatus(StatusConstant.ENABLE);
+
+        newEmployee.setCreateTime(LocalDateTime.now());
+        newEmployee.setUpdateTime(LocalDateTime.now());
+
+        newEmployee.setCreateUser(10L);
+        newEmployee.setUpdateUser(10L);
+
+        employeeMapper.insert(newEmployee);
+    }
+
 
 }
